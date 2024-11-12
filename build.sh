4c06eed7451a14396c63e7a510fbce4f0df60424
Mani25599 <kmanikandan25599@gmail.com>
2024-11-12 13:06:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #login into DockerHub:
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
 
 #stopping existing container:
-docker stop react
-docker rm react
+sudo docker stop react
+sudo docker rm react
 
 #building a image:
-docker build -t react-ci/cd .
+sudo docker build -t react-ci/cd .
 
 #running a container from the created image:
-docker run -d -it --name react -p 81:81 react-ci/cd
+sudo docker run -d -it --name react -p 81:80 react-ci/cd
 
 #pushing the image to dockerhub:
 docker tag react-ci/cd mani25599/react-app
